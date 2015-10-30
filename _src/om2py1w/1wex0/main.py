426074987c76967d8d8d9c4e3e5d2f8e7dbc3381
scottming <xiaowan5219@gmail.com>
2015-10-30 09:28:02 +0800

    搞定基本的读写、持续交互功能。

--- _src/om2py1w/1wex0/main.py
+++ _src/om2py1w/1wex0/main.py
@@ -1,4 +1,20 @@
-luoxiong
-nihao
-4+nihao
-iiiii
+# _*_ coding:utf-8 _*_
+
+from sys import argv #获取外部命令的参数
+script, wr = argv
+
+if wr == "w": 
+	target = open('daily.txt','a') 
+	print "我要开始装逼了"
+	while True:
+		s = raw_input('Enter something : ')
+		target.write(s)
+		target.write('\n')
+		if s == 'quit':
+			break
+	target.close()
+	print 'Done'	
+elif wr == "r":
+	print "It's reading"
+	target = open("daily.txt")
+	print target.read()
